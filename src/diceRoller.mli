f34ff7195bd6cc62fc2e8c5f2e1bea87541893f2
Chris Yocum <c.yocum@ed.ac.uk>
2013-03-18 12:07:23 +0000

    forgot to remove from mli file

--- src/diceRoller.mli
+++ src/diceRoller.mli
@@ -1,5 +1,5 @@
 (*
-Copyright 2011 Christopher Guy Yocum 
+Copyright 2011 Christopher Guy Yocum
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -14,9 +14,6 @@ Copyright 2011 Christopher Guy Yocum
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 *)
-
-exception BadDiceRoll of string
-exception ZeroSides of string
 
 type r
 
